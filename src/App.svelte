e02a41ab862d563c63bc4534b10c3c7b1e84a591
Dylan Reed <dylan.reeder5@gmail.com>
2024-03-15 18:27:10 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import svelteLogo from "./assets/svelte.svg";
+    import viteLogo from "/vite.svg";
+    import Counter from "./lib/Counter.svelte";
+    import classOneDesign from "./assets/class-1-design.mp3";
+
+    // get audio tag to play in background when a mobile device is off
+    const audio = document.getElementById("class-audio") as HTMLAudioElement;
+    if (audio) {
+        audio.play();
+    }
+</script>
+
+<main>
+    <audio id="class-audio" controls>
+        <source src={classOneDesign} type="audio/mpeg" />
+    </audio>
+</main>
